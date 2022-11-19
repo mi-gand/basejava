859eabcfd4aec67be13776dbd9efc687343ecaf2
yludivin <mi.gand@yandex.ru>
2022-11-19 12:38:49 +0300

    fix: fix metods in ArrayStorage-3

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
-    private int size = 0;
+    private int size;
 
     public void clear() {
-        Arrays.fill(storage, null);
-        this.size = 0;
+        Arrays.fill(storage, 0, size - 1, null);
+        size = 0;
     }
 
     public void save(Resume r) {
@@ -17,31 +17,27 @@ public class ArrayStorage {
             return;
         }
         storage[size] = r;
-        this.size++;
+        size++;
     }
 
     public Resume get(String uuid) {
-        Resume r = new Resume();
-        for (int i = 0; i < this.size; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
-                r = storage[i];
+                return storage[i];
             }
         }
-        return r;
+        return null;
     }
 
     public void delete(String uuid) {
-        int deletedIndex = -1;
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
-                deletedIndex = i;
-                int lastResumeIndex = size - 1;
-                storage[deletedIndex] = storage[lastResumeIndex];
-                storage[lastResumeIndex] = null;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
                 break;
             }
         }
-        this.size--;
     }
 
     /**
@@ -49,10 +45,10 @@ public class ArrayStorage {
      */
 
     public Resume[] getAll() {
-        return Arrays.copyOf(this.storage, this.size);
+        return Arrays.copyOf(storage, size);
     }
 
     public int getSize() {
-        return this.size;
+        return size;
     }
 }
